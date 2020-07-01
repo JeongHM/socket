8ac53efc9bcbbcc11fb92c6e8223983600dd0eee
JeongHM <kjhm0607@gmail.com>
2020-07-02 00:44:31 +0900

    [feat] 채팅 뷰 적용

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/room', (req, res, next) => {
+  res.render('chat')
+})
+
 module.exports = router;
