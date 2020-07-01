61de0a82e0edbbd218a8e7739206f22eac3e84b9
JeongHM <kjhm0607@gmail.com>
2020-07-02 00:43:21 +0900

    [Refac] 소켓 뷰 템플릿 적용 ejs
    - 잠시 적용으로 다시 원복 할 예정

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,8 @@ const app = express();
 app.io = require('socket.io')();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(cors());
 app.use(logger('dev'));
@@ -25,19 +26,51 @@ app.use('/users', usersRouter);
 app.use('/rooms', roomsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+app.get('/', (req, res) => {
+  res.render('chat');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// 소켓 함수 구현
+app.io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+  socket.on('leaveRoom', (roomNum, name) => {
+    socket.leave(roomNum, () => {
+      console.log(name + ' leave a ' + roomNum);
+      app.io.to(roomNum).emit('leaveRoom', roomNum, name);
+    });
+  });
+
+
+  socket.on('joinRoom', (roomNum, name) => {
+    socket.join(roomNum, () => {
+      console.log(name + ' join a ' + roomNum);
+      app.io.to(roomNum).emit('joinRoom', roomNum, name);
+    });
+  });
+
+
+  socket.on('chat message', (roomNum, name, msg) => {
+    app.io.to(roomNum).emit('chat message', name, msg);
+  });
 });
 
 module.exports = app;
